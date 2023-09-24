4c174f9ebd0e15770fc01593874f9056d9f2902b
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2023-09-24 18:40:45 -0300

    Correção da questão 3

--- Main.cpp
+++ Main.cpp
@@ -10,25 +10,24 @@ void coletandoDados() {
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution<>intDistribution(1, 3);
 
+	
+
 	switch (intDistribution(gen))
 	{
 	case 1:
 		tipoDado = "Vetor";
-		std::this_thread::sleep_for(std::chrono::seconds(2));
 
 		ProcessandoDados::processaDevagar(GeraValores::geraVetor(1000), GeraValores::geraVetor(3000), GeraValores::geraVetor(5000), GeraValores::geraVetor(10000));
 
 		break;
 	case 2:
 		tipoDado = "Lista encadeada";
-		std::this_thread::sleep_for(std::chrono::seconds(2));
 
 		ProcessandoDados::processaDevagar(GeraValores::geraListaEncadeada(1000), GeraValores::geraListaEncadeada(3000), GeraValores::geraListaEncadeada(5000), GeraValores::geraListaEncadeada(10000));
 
 		break;
 	case 3:
 		tipoDado = "Lista duplamente encadeada";
-		std::this_thread::sleep_for(std::chrono::seconds(2));
 
 		ProcessandoDados::processaDevagar(GeraValores::geraListaDuplamenteEncadeada(1000), GeraValores::geraListaDuplamenteEncadeada(3000), GeraValores::geraListaDuplamenteEncadeada(5000), GeraValores::geraListaDuplamenteEncadeada(10000));
 		break;
--- ProcessandoDados.cpp
+++ ProcessandoDados.cpp
@@ -3,28 +3,145 @@
 long long ProcessandoDados::randomTimer() {
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_int_distribution<>intDistribution(1000, 10000);
+	std::uniform_int_distribution<>intDistribution(0, 10);
 
 	return (long long)intDistribution(gen);
 }
 void ProcessandoDados::processaDevagar(std::vector<int>conjunto1, std::vector<int>conjunto2, std::vector<int>conjunto3, std::vector<int>conjunto4) {
 
+	std::this_thread::sleep_for(std::chrono::seconds(2));
 	std::cout << "Primeiro conjunto" << std::endl;
 	for (int i : conjunto1)
 	{
 		std::cout << "Valor atual: " << i << " " << std::endl;
 
-		
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
 
-		std::cout << "Processamento concluido, o valor sera removido em instantes..." << std::endl;;
+		std::cout << "Pagamento concluido" << std::endl;;
+
+	}
+	std::cout << "Segundo conjunto" << std::endl;
+	for (int i : conjunto2)
+	{
+		std::cout << "Valor atual: " << i << " " << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;;
+
+	}
+	std::cout << "Terceiro conjunto" << std::endl;
+	for (int i : conjunto3)
+	{
+		std::cout << "Valor atual: " << i << " " << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;;
+
+	}
+	std::cout << "Quarto conjunto" << std::endl;
+	for (int i : conjunto4)
+	{
+		std::cout << "Valor atual: " << i << " " << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;;
 
 	}
 
 }
 void ProcessandoDados::processaDevagar(LinkedList conjunto1, LinkedList conjunto2, LinkedList conjunto3, LinkedList conjunto4) {
 
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	std::vector<int> teste1;
+	std::vector<int> teste2;
+	std::vector<int> teste3;
+	std::vector<int> teste4;
+	
+	std::cout << "Primeiro conjunto" << std::endl;
+	for (int i = 0; i < conjunto1.size(); i++)
+	{
+		std::cout << "Valor atual: " << conjunto1.getValueAtPosition(i) << " " << std::endl;
 
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;
+
+	}
+	std::cout << "Segundo conjunto" << std::endl;
+	for (int i = 0; i < conjunto2.size(); i++)
+	{
+		std::cout << "Valor atual: " << conjunto2.getValueAtPosition(i) << " " << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;
+
+	}
+	std::cout << "terceiro conjunto" << std::endl;
+	for (int i = 0; i < conjunto3.size(); i++)
+	{
+		std::cout << "Valor atual: " << conjunto3.getValueAtPosition(i) << " " << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;
+
+	}
+	std::cout << "Quarto conjunto" << std::endl;
+	for (int i = 0; i < conjunto4.size(); i++)
+	{
+		std::cout << "Valor atual: " << conjunto4.getValueAtPosition(i) << " " << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;
+
+	}
 }
 void ProcessandoDados::processaDevagar(DoublyLinkedList conjunto1, DoublyLinkedList conjunto2, DoublyLinkedList conjunto3, DoublyLinkedList conjunto4) {
 
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	std::cout << "Primeiro conjunto" << std::endl;
+	for (int i = 0; i < conjunto1.size(); i++)
+	{
+		std::cout << "Valor atual: " << conjunto1.getValueAtPosition(i) << " " << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;
+
+	}
+	std::cout << "Segundo conjunto" << std::endl;
+	for (int i = 0; i < conjunto2.size(); i++)
+	{
+		std::cout << "Valor atual: " << conjunto2.getValueAtPosition(i) << " " << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;;
+
+	}
+	std::cout << "terceiro conjunto" << std::endl;
+	for (int i = 0; i < conjunto3.size(); i++)
+	{
+		std::cout << "Valor atual: " << conjunto3.getValueAtPosition(i) << " " << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;;
+
+	}
+	std::cout << "Quarto conjunto" << std::endl;
+	for (int i = 0; i < conjunto4.size(); i++)
+	{
+		std::cout << "Valor atual: " << conjunto4.getValueAtPosition(i) << " " << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(randomTimer()));
+
+		std::cout << "Pagamento concluido" << std::endl;;
+
+	}
 }
