5d50d00d4558c02543fe74c69dd8b4d25c0b0462
Lucas Kogima <lucasmorihara@hotmail.com>
2023-09-30 01:53:03 -0300

    commit: acrescentando pilha

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,7 @@
 #include "geraValores.hpp"
 #include "processandoDados.hpp"
 
+
 std::string tipoDado = "";
 
 
@@ -13,10 +14,10 @@ int main()
 	std::cout << "Por favor, aguarde um instante que as informacoes ja irao ser processadas..." << std::endl;
 
 	ProcessandoDados process;
-	std::vector<int> random1 = GeraValores::geraVetor(100);
-	std::vector<int> random2 = GeraValores::geraVetor(100);
-	std::vector<int> random3 = GeraValores::geraVetor(100);
-	std::vector<int> random4 = GeraValores::geraVetor(100);
+	std::vector<int> random1 = GeraValores::geraVetor(10);
+	std::vector<int> random2 = GeraValores::geraVetor(10);
+	std::vector<int> random3 = GeraValores::geraVetor(10);
+	std::vector<int> random4 = GeraValores::geraVetor(10);
 
 	process.juntandoDados(random1, random2, random3, random4);
 	std::this_thread::sleep_for(std::chrono::seconds(2));
@@ -39,6 +40,17 @@ int main()
 
 	if (confirm == "s" || confirm == "S") {
 		process.processandoDadosParalelo();
+	}
+	else {
+		exit;
+	}
+	
+	std::cout << std::endl;
+	std::cout << "Deseja continuar? (s/n)" << std::endl;
+	std::cin >> confirm;
+
+	if (confirm == "s" || confirm == "S") {
+		process.processandoDadosParaleloPilha();
 	}
 	else {
 		exit;
--- processandoDados.hpp
+++ processandoDados.hpp
@@ -7,22 +7,29 @@
 #include "LinkedList.hpp"
 #include "doublyLinkedList.hpp"
 #include "threadPool.hpp"
+#include "pilha.hpp"
 
 class ProcessandoDados
 {
 private:
 	static long long randomTimer();
-	void lendoDados(int value,std::string type);
+	void lendoDados(int value, std::string type);
 	void lendoListaSimples();
 	void lendoListaEncadeada();
 	void lendoVetor();
+	void lendoVetor2();
+	void processaVetor();
+	void lendoPilha();
 	std::vector<int> vector;
+	std::vector<int> vector2;
 	LinkedList linkedList;
 	DoublyLinkedList doublyLinkedList;
+	Pilha pilha;
 
 public:
 	void processandoDados();
 	void processandoDadosParalelo();
+	void processandoDadosParaleloPilha();
 	void juntandoDados(std::vector<int>conjunto1, std::vector<int>conjunto2, std::vector<int>conjunto3, std::vector<int>conjunto4);
 };
 
